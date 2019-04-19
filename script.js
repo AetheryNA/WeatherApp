8d2aa9d2a0f409b55a44aa508e36a1d46c1f97a8
Aether Ahri <aetheryna.naveedahamed@gmail.com>
2019-04-19 21:06:32 +0530

    A few changes

--- script.js
+++ script.js
@@ -1,59 +1,57 @@
-window.addEventListener('load', () => {
-    let long;
-    let lat;
-    let temperatureDesc = document.querySelector('.temperature-description');
-    let temperatureDeg = document.querySelector('.temperature-degrees');
-    let locationTimezone = document.querySelector('.location-timezone');
-    let temperatureSection = document.querySelector('.temperature');
-    let temperatureSpan = document.querySelector('.temperature span');
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let temperatureDesc = document.querySelector(".temperature-description");
+  let temperatureDeg = document.querySelector(".temperature-degrees");
+  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  let temperatureSpan = document.querySelector(".temperature span");
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-            const proxy = `https://cors-anywhere.herokuapp.com/`;
-            const api = `${proxy}https://api.darksky.net/forecast/af460b9be761f42a2f3f3f914bcf0543/${lat},${long}`;
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
+      const api = `${proxy}https://api.darksky.net/forecast/af460b9be761f42a2f3f3f914bcf0543/${lat},${long}`;
+      console.log(api);
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
 
-            fetch(api)
-            .then(response => {
-                return response.json();
-            })
+        .then(data => {
+          const { temperature, summary, icon } = data.currently;
 
-            .then(data => {
-                const {temperature, summary, icon} = data.currently;
+          //Set DOM elements
 
-                //Set DOM elements
+          temperatureDeg.textContent = temperature;
+          temperatureDesc.textContent = summary;
+          locationTimezone.textContent = data.timezone;
 
-                temperatureDeg.textContent = temperature;
-                temperatureDesc.textContent = summary;
-                locationTimezone.textContent = data.timezone;
+          //FORMULA for C
+          let celcius = (temperature - 32) * (5 / 9);
 
-                //FORMULA for C
-                let celcius = (temperature - 32) * (5 / 9);
+          setIcons(icon, document.querySelector(".icon"));
 
-                setIcons (icon, document.querySelector('.icon'));
-
-                temperatureSection.addEventListener('click', () => {
-                    if( temperatureSpan.textContent === "F") {
-                        temperatureSpan.textContent = "C";
-                        temperatureDeg.textContent = Math.floor(celcius);
-                    }
-
-                    else {
-                        temperatureSpan.textContent = "F";
-                        temperatureDeg.textContent = temperature;
-                    }
-                });
-            });
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDeg.textContent = Math.floor(celcius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDeg.textContent = temperature;
+            }
+          });
         });
-    }
+    });
+  }
 
-    function setIcons (icon, iconID){
-        const skycons = new Skycons({color: "white"});
-        const currentIcon = icon.replace(/-/g , "_").toUpperCase();
-        skycons.play();
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
 
-        return skycons.set(iconID, Skycons[currentIcon]);
-    }
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
 });
